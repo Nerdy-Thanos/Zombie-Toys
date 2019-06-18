5c9d8fbc4b34577d221bdcd7c4c28569086cb956
Nerdy-Thanos <47348592+Nerdy-Thanos@users.noreply.github.com>
2019-06-18 14:49:02 +0530

    Player Scripts updated
    Player health scrpit to count and update player health on damage taken. player movement to enable movement controls. player shooting to enable shooting effects and damage the enemies.

---
+++ PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float Position = 0.0f;
+	void start()
+	{
+		Player.Set.Position = Vector3 (0.0f, 0.0f, 0.0f);
+	}
+	void update()
+	{
+		Player.Transform.Position = Random (Cursor.SetCursor);
+	}
+}
